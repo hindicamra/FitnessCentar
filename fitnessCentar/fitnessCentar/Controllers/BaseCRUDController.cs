a8d7a33ddedc0a0d9e6f352899dd18661980801f
hindicamra <amra.hindic@edu.fit.ba>
2023-11-13 21:28:42 +0100

    fixed

--- fitnessCentar/fitnessCentar/Controllers/BaseCRUDController.cs
+++ fitnessCentar/fitnessCentar/Controllers/BaseCRUDController.cs
@@ -7,23 +7,25 @@ namespace fitnessCentar.Controllers
 
     public class BaseCRUDController<T, TSearch, TInsert, TUpdate> : BaseController<T, TSearch> where T : class where TSearch : class
     {
-        protected readonly ICRUDService<T, TSearch, TInsert, TUpdate> service;
+        protected readonly ICRUDService<T, TSearch, TInsert, TUpdate> _service;
         protected readonly ILogger<BaseController<T, TSearch>> _logger;
 
         public BaseCRUDController(ILogger<BaseController<T, TSearch>> logger, ICRUDService<T, TSearch, TInsert, TUpdate> service) : base(logger, service)
         {
+            _service = service;
+            _logger = logger;
         }
 
         [HttpPost]
         public virtual async Task<T> Insert([FromBody] TInsert insert)
         {
-            return await service.Insert(insert);
+            return await _service.Insert(insert);
         }
 
         [HttpPut("{id}")]
         public virtual async Task<T> Update(int id, [FromBody] TUpdate update)
         {
-            return await service.Update(id, update);
+            return await _service.Update(id, update);
         }
     }
 }
