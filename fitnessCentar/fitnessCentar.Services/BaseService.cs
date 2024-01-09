3f0f076a07a461f256ed100021f82cba44179713
hindicamra <126388697+hindicamra@users.noreply.github.com>
2024-01-09 09:14:01 +0100

    Update BaseService.cs

--- fitnessCentar/fitnessCentar.Services/BaseService.cs
+++ fitnessCentar/fitnessCentar.Services/BaseService.cs
@@ -2,7 +2,75 @@
 using fitnessCentar.Model;
 using fitnessCentar.Model.SearchObjects;
 using fitnessCentar.Services.Database;
+using Microsoft.EntityFrameworkCore;using AutoMapper;
+using fitnessCentar.Model;
+using fitnessCentar.Model.SearchObjects;
+using fitnessCentar.Services.Database;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitnessCentar.Services
+{
+    public class BaseService<T, TDb, TSearch>:IService<T, TSearch> where T : class where TDb : class where TSearch : BaseSearchObject
+    {
+        protected FitnessCentarContext _context;
+
+        public IMapper _mapper { get; set; }
+
+        public BaseService(FitnessCentarContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<PagedResult<T>> Get(TSearch search)
+        {
+            var query = _context.Set<TDb>().AsQueryable();
+
+            PagedResult<T> result = new PagedResult<T>();
+
+           
+
+            query = AddFilter(query, search);
+            query = AddInclude(query, search);
+
+            result.Count = await query.CountAsync();
+
+            if (search?.Page.HasValue == true && search?.PageSize.HasValue == true)
+            {
+                query = query.Take(search.PageSize.Value).Skip(search.Page.Value * search.PageSize.Value);
+            }
+            var list = await query.ToListAsync();
+
+            result.Result = _mapper.Map<List<T>>(list);
+            return result;
+
+        }
+        
+        public virtual IQueryable<TDb> AddInclude(IQueryable<TDb> query, TSearch? search = null)
+        {
+            return query;
+        }
+
+        public virtual IQueryable<TDb> AddFilter(IQueryable<TDb> query, TSearch? search = null)
+        {
+            return query;
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            var entity = await _context.Set<TDb>().FindAsync(id);
+
+            return _mapper.Map<T>(entity);
+        }
+    }
+}
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
