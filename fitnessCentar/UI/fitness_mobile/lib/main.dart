ddf4010a11cf5fc94f49533d50ff990459bdc360
Amra Hindic <amra.hindic@edu.fit.ba>
2024-02-09 17:30:01 +0100

    Added getx GetMaterialApp to main.lib fixed android problems

--- fitnessCentar/UI/fitness_mobile/lib/main.dart
+++ fitnessCentar/UI/fitness_mobile/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
-  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
@@ -14,28 +12,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return const GetMaterialApp(
+      home: MyHomePage(
+        title: 'Title',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      getPages: [
+        // GetPage(
+        //   participatesInRootNavigator: true,
+        //   name: '/first',
+        //   page: () => const MyHomePage(
+        //     title: 'Title',
+        //   ),
+        // ),
+      ],
+      debugShowCheckedModeBanner: false,
     );
   }
 }
