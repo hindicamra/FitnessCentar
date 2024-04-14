0ec1ebb96fe9af9c0a0b4c6867bf082bf88d80f9
hindicamra <amra.hindic@edu.fit.ba>
2024-04-14 10:54:14 +0200

    Update ClanarinaController.cs

--- fitnessCentar/fitnessCentar/Controllers/ClanarinaController.cs
+++ fitnessCentar/fitnessCentar/Controllers/ClanarinaController.cs
@@ -10,13 +10,29 @@ namespace fitnessCentar.Controllers
 {
     [ApiController]
     [Authorize(Roles = "Admin,Korisnik")]
-    [ApiExplorerSettings(IgnoreApi = true)]
+    
     public class ClanarinaController : BaseCRUDController<Model.Clanarina, Model.SearchObjects.ClanarinaSearchObject, ClanarinaInsertRequest, ClanarinaUpdateRequest>
     {
 
         public ClanarinaController(ILogger<BaseController<Model.Clanarina, Model.SearchObjects.ClanarinaSearchObject>> logger, IClanarinaService service)
                 : base(logger, service)
         {
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public override Task<Clanarina> Insert([FromBody] ClanarinaInsertRequest insert)
+        {
+            return base.Insert(insert);
+        }
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public override Task<Clanarina> Update(int id, [FromBody] ClanarinaUpdateRequest update)
+        {
+            return base.Update(id, update);
+        }
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public override Task Delete(int id)
+        {
+            return base.Delete(id);
         }
     }
 }
