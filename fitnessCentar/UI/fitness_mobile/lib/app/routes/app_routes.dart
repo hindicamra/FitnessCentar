afbd264c8f0d2930cf91253acde7a7de7707396a
hindicamra <amra.hindic@edu.fit.ba>
2024-04-15 20:41:41 +0200

    Removed old flutter

--- fitnessCentar/UI/fitness_mobile/lib/app/routes/app_routes.dart
+++
@@ -1,9 +0,0 @@
-/// App routes names
-class AppRoutes {
-  static const login = '/login';
-  static const register = '/register';
-  static const naslovna = '/naslovna';
-  static const korpa = '/korpa';
-  static const dodajRecenziju = '/dodajRecenziju';
-  static const urediProfil = '/urediProfil';
-}
--- fitnessCentar/UI/fitness_mobile/lib/app/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-class AppConstants {
-  static const baseUrl = '/';
-  static DateFormat dateFormat = DateFormat("dd/MM/yyyy");
-}
